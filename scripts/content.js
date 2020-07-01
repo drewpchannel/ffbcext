7b5012a1b5e5fb8280e1070625233623ed99c940
DP <drewpcchannel@gmail.com>
2020-07-01 15:47:57 -0400

    pulls player names, team, pos from page

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,25 @@
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-    console.log(document.getElementById('ys-playerlist-panel'));
-    sendResponse({tableInfo: document.getElementById('ys-playerlist-panel'), success: true});
+    var playerHTML = document.getElementsByClassName('infinite-scroll-list')[0].rows;
+    var playerArray = [];
+    for (key in playerHTML) { playerArray.push(playerHTML[key].cells) };
+    var playerArrayText = [];
+    playerArray.forEach((playerData, index) => { 
+    		if (playerArray[index]) {
+    			if (playerArray[index][2]) { 
+    				if (playerArray[index][2].children[1].innerText.length < 2) {
+    					playerArrayText.push([
+    						playerArray[index][2].children[2].innerText,
+    						playerArray[index][2].children[3].innerText
+    					]);
+    				} else {
+    				    playerArrayText.push([
+    						playerArray[index][2].children[1].innerText,
+    						playerArray[index][2].children[2].innerText
+    					]);
+                    }
+    			}
+    		}
+    	});
+    sendResponse({tableInfo: playerArrayText, success: true});
 });
 
