3f6510b56e3a381ff26b8e5a549b43692b8e838d
drewpchannel <drewpcchannel@gmail.com>
2020-07-21 14:38:54 -0400

    player data in tables, boots changed to url for now

--- scripts/extension.js
+++ scripts/extension.js
@@ -6,9 +6,45 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             //possibly check for false and resend with message that content is loading
             chrome.tabs.sendMessage(tabs[0].id, {data:'McNuggets'}, (response) => {
-                console.log(response);
+                createPlayersTbl(response.tableInfo);
+                console.log('loading data');
             });
         });
     });
 });
 
+function createPlayersTbl(response) {
+    //var response = [["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"]];
+    var body = document.getElementsByTagName("body")[0];
+    var table = document.createElement("playersTable");
+    table.className = "table";
+
+    var thead = document.createElement("thead");
+    var tbody = document.createElement("tbody");
+    var headRow = document.createElement("tr");
+
+    ["Player", "Pos - Team"].forEach((elem) => {
+        console.log('foreach header');
+        var th = document.createElement("th");
+        th.appendChild(document.createTextNode(elem));
+        headRow.appendChild(th);
+    });
+    thead.appendChild(headRow);
+    table.appendChild(thead); 
+
+    if (response) {
+        response.forEach((elem) => {
+            var tr = document.createElement("tr");
+            for (var o in elem) {
+                var td = document.createElement("td");
+                td.appendChild(document.createTextNode(elem[o]));
+                tr.appendChild(td);
+            }
+            tbody.appendChild(tr);
+        });
+        table.appendChild(tbody);
+    body.appendChild(table);
+    } else {
+        var response = [["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"]];
+    }
+}
