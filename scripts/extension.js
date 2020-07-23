556e5d00698e0fdf4b78386a8e6aef8aa75fc3b1
drewpchannel <drewpcchannel@gmail.com>
2020-07-23 13:31:35 -0400

    removing drafted players from table

--- scripts/extension.js
+++ scripts/extension.js
@@ -1,17 +1,31 @@
-function getCSVFile() {
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('status').textContent = "Extension loaded";
+    var button = document.getElementById('changelinks');
+    button.addEventListener('click', function () {
+        document.getElementById('status').innerHTML = 'Trying to find table...';
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            //possibly check for false and resend with message that content is loading
+            chrome.tabs.sendMessage(tabs[0].id, {data:'McNuggets'}, (responseFromSite) => {
+                getCSVFile(responseFromSite.tableInfo);
+            });
+        });
+    });
+});
+
+function getCSVFile(responseFromSite) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', chrome.extension.getURL('rankings/fpec.csv'), true);
     xhr.onreadystatechange = function()
     {
         if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
         {
-            createFileArray(xhr.response);
+            createFileArray(xhr.response, responseFromSite);
         }
     };
     xhr.send();
 }
 
-function createFileArray(file) {
+function createFileArray(file, responseFromSite) {
     var fileNoNewLines = file.replace( /[\r\n\"]+/gm, "" );
     var arrayPlayerNames = [];
     var fileArray = fileNoNewLines.split(',');
@@ -20,26 +34,22 @@ function createFileArray(file) {
             arrayPlayerNames.push(elem);
         }
     });
-    console.log(arrayPlayerNames);
-    createPlayersTbl(arrayPlayerNames);
+    checkForDrafted(arrayPlayerNames, responseFromSite);
 }
 
-getCSVFile();
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('status').textContent = "Extension loaded";
-    var button = document.getElementById('changelinks');
-    button.addEventListener('click', function () {
-        document.getElementById('status').innerHTML = 'Trying to find table...';
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            //possibly check for false and resend with message that content is loading
-            chrome.tabs.sendMessage(tabs[0].id, {data:'McNuggets'}, (response) => {
-                createPlayersTbl(response.tableInfo);
-                console.log('loading data');
-            });
+function checkForDrafted(arrayPlayerNames, responseFromSite) {
+    console.log(responseFromSite);
+    console.log(arrayPlayerNames);
+    var undraftedPlayers = [];
+    responseFromSite.forEach((elem, ind) => {
+        arrayPlayerNames.forEach((elemPN, indPN) => {
+            if (responseFromSite[ind][0] == arrayPlayerNames[indPN]) {
+                undraftedPlayers.push(arrayPlayerNames[indPN]);
+            }
         });
     });
-});
+    createPlayersTbl(undraftedPlayers);
+}
 
 function createPlayersTbl(response) {
     var body = document.getElementsByTagName("body")[0];
@@ -62,16 +72,14 @@ function createPlayersTbl(response) {
     if (response) {
         response.forEach((elem, ind) => {
             var tr = document.createElement("tr");
-            for (var o in elem) {
-                var td = document.createElement("td");
-                td.appendChild(document.createTextNode(elem[o]));
-                tr.appendChild(td);
-            }
+            var td = document.createElement("td");
+            td.appendChild(document.createTextNode(elem));
+            tr.appendChild(td);
             tbody.appendChild(tr);
         });
         table.appendChild(tbody);
     body.appendChild(table);
     } else {
-        var response = [["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"],["Henry", "TE"], ["bob", "wr"]];
+        var response = [["response empty", "no data"]];
     }
 }
